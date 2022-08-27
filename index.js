d1387ef8e4d6212ad16f19534a1cfe8c7b7ee392
bui huy bach <bachkame@gmail.com>
2022-08-28 04:36:34 +0700

    them route login, register

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require('./routes/auth');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("ket noi db thanh cong");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/v1/auth", authRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("sever is running at port ",process.env.PORT || 5000 );
+});
---
+++ routes/auth.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const Router = express.Router();
+const UserSchema = require('../model/user');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//Dang ki POST
+
+Router.post("/register", async (req, res) => {
+  const user = req.body;
+
+  if(!user.phone || !user.password){
+    return res.status(400).json({success: false, message: 'Chưa nhập thông tin'})
+  }
+  
+  try{
+    const existUser = await UserSchema.findOne({phone: user.phone});
+    if(existUser){
+      return res.status(400).json({success: false, message: 'Số điện thoại đã được đăng kí'});
+    }
+
+    const hashPassword = await argon2.hash(user.password);
+    const newUser = new UserSchema({...user, password: hashPassword});
+    await newUser.save();
+
+    const accessToken = jwt.sign(
+      {userId: newUser._id},
+      process.env.ACCESS_TOKEN_SECRET
+    )
+
+    return res.status(200).json({
+      success: true,
+      message: "Tạo tài khoản thành công",
+      data: null,
+      accessToken: accessToken
+    })
+
+  }catch(err){
+    console.log(err);
+    return res.status(400).json({success: false, message: 'Đăng kí không thành công'});
+  }
+})
+
+Router.post("/login", async (req, res) =>{
+  const user = req.body;
+  if(!user.phone || !user.password){
+    return res.status(400).json({success: false, message: 'Chưa nhập thông tin'})
+  }
+  try{
+    const existUser = await UserSchema.findOne({phone: user.phone});
+    if(!existUser){
+      return res.status(400).json({success: false, message: 'Tài khoản chưa tồn tại'});
+    }
+
+    const isValidPassword = await argon2.verify(existUser.password, user.password);
+    if(isValidPassword){
+      return(res.status(200).json({success: true, message: "Đăng nhập thành công", data: null}))
+    }else{
+      return res.status(400).json({success: false, message: 'Đăng nhập không thành công'});
+    }
+
+
+  }catch(err){
+    console.log(err);
+    return res.status(400).json({success: false, message: 'Đăng nhập không thành công'});
+  }
+})
+
+module.exports = Router;
