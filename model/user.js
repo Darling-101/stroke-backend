e8f0d79f3922abef11fd9faebcb0b464ed8069e4
bui huy bach <bachkame@gmail.com>
2022-08-31 17:05:57 +0700

    sua model user

--- model/user.js
+++ model/user.js
@@ -3,7 +3,9 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   phone: {type:String, require: true},
-  password: {type: String, require: true}
+  password: {type: String, require: true},
+  position: {type: Array, default: []},
+  RelationshipPhone: {type:Array, default: []}
 })
 
 module.exports = mongoose.model("User", UserSchema);
