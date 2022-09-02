8b8c5f5233521aef24744a5cbb305487949fea95
bui huy bach <bachkame@gmail.com>
2022-09-02 13:44:45 +0700

    test

--- model/user.js
+++ model/user.js
@@ -5,7 +5,8 @@ const UserSchema = new Schema({
   phone: {type:String, require: true},
   password: {type: String, require: true},
   position: {type: Array, default: []},
-  RelationshipPhone: {type:Array, default: []}
+  RelationshipId: {type:Array, default: []},
+  SOS: {type: Boolean, default: false}
 })
 
 module.exports = mongoose.model("User", UserSchema);
