f01336362702570cfbf4cc839b2b681200435fd8
bui huy bach <bachkame@gmail.com>
2022-09-26 19:58:53 +0700

    fix

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,6 +3,10 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
 
+  if(!accessToken){
+    return;
+  }
+
   const accessToken = authHeader.split(' ')[1];
   if (!accessToken) {
     return res.status(401).json({ success: false, message: "Token not found" });
