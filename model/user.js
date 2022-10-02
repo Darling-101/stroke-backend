47809b48574a542a449bd3e8d63fcebee9d2c0de
bui huy bach <bachkame@gmail.com>
2022-10-02 09:37:25 +0700

    them field model user

--- model/user.js
+++ model/user.js
@@ -1,22 +1,36 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  fullname: { type: String },
-  phone: { type: String, require: true },
-  sex: { type: String },
-  birthOfDate: {type: String},
-  password: { type: String, require: true },
-  position: { type: Array, default: [] },
-  RelationshipId: { type: Array, default: [] },
-  SOS: { type: Boolean, default: false },
-  role: { type: String, default: "user" },
-  weight: { type: Number, default:0 },
-  height: { type: Number, default: 0 },
-  heartSpeed: { type: Number, default:0 },
-  tamThu: {type:Number, default:0 },
-  tamTuong: {type: Number, default:0 },
-  workPlace: {type:String, default: ''}
-},{timestamps:true});
+const UserSchema = new Schema(
+  {
+    fullname: { type: String },
+    phone: { type: String, require: true },
+    sex: { type: String },
+    birthOfDate: { type: String },
+    password: { type: String, require: true },
+    position: { type: Array, default: [] },
+    RelationshipId: { type: Array, default: [] },
+    SOS: { type: Boolean, default: false },
+    role: { type: String, default: "user" },
+    weight: { type: Number, default: 0 },
+    height: { type: Number, default: 0 },
+    heartSpeed: { type: Number, default: 0 },
+    tamThu: { type: Number, default: 0 },
+    tamTuong: { type: Number, default: 0 },
+    workPlace: { type: String, default: "" },
+    daily: {
+      type: Object,
+      default: {
+        smoking: 0,
+        blood_sugar: 0,
+        blood_pressure: 0,
+        sleepingTime: 0,
+        feeling: 0,
+        chucNangThiGiac: "",
+      },
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", UserSchema);
